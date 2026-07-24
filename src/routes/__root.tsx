7444cb0aa7ca581d06c954656ac2a7d5338ce4d3
CodewithShahriar <abid.shahriar.23@gmail.com>
2026-07-24 18:14:57 +0600

    ui update- mobile device layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -83,7 +83,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "stylesheet", href: appCss },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
       { rel: "manifest", href: "/manifest.webmanifest" },
-      { rel: "apple-touch-icon", href: "/cinejournal.jpg", type: "image/jpeg" },
+      { rel: "apple-touch-icon", href: "/cinemacity.jpg", type: "image/jpeg" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;600;700&family=Inter:wght@400;500;600;700&display=swap" },
